261e448b8ea161a1bbcd3951b2471c50f34b5afd
Jonathan Rocha <jonathansantanarocha27@gmail.com>
2022-04-03 10:48:54 -0300

    ajustando javascript e styles em geral

--- Assets/javascript/js.js
+++ Assets/javascript/js.js
@@ -2,7 +2,7 @@
 
 
 // Selector menu mobile
-document.querySelector('.menumob').addEventListener("click", function() {
+document.querySelector('.menumob').addEventListener("click", function () {
 
     if (document.querySelector(" nav").style.display == 'flex') {
 
@@ -23,3 +23,67 @@ document.querySelector('.menumob').addEventListener("click", function() {
     }
 
 });
+
+
+select = (e) => document.querySelector(e)
+let btleft = select('.banner-controls--left')
+let btrigth = select('.banner-controls--right ')
+let banner = select('.banner-carrossel .banner-1')
+
+
+
+btleft.addEventListener('click', () => { 
+    if(banner.style.width == '100%') {
+        banner.style.width = '0'
+    }else {
+        banner.style.width = '100%'
+    }
+})
+
+btrigth.addEventListener('click', () => {
+   
+
+    if(banner.style.width == '100%') {
+        banner.style.width = '0'
+    }else {
+        banner.style.width = '100%'
+    }
+})
+let acess = document.querySelector('.acess ')
+
+
+
+acess.addEventListener('click', (e) => {
+
+    document.querySelectorAll('.about-us li').forEach((e) => {
+        e.style.display = 'none';
+    })
+
+    document.querySelectorAll('.about-us ul svg').forEach((e) => {
+        e.style.transform = "rotate(0deg)"
+        e.style.transitionDuration = '0s'
+    })
+
+    let abrir = acess.querySelectorAll('.about-us')
+
+    let open = e.target.parentNode
+    if (open.querySelector('li').style.display == 'none') {
+
+        open.querySelector('svg').style.transform = "rotate(180deg)"
+        open.querySelector('svg').style.transitionDuration = "0.15s"
+        
+        open.querySelectorAll('li').forEach((a) => {
+            a.style.display = 'block';
+        })
+
+    } else {
+
+        open.querySelectorAll('li').forEach((a) => {
+            a.style.display = 'none';
+            console.log(open)
+        })
+
+    }
+
+})
+
